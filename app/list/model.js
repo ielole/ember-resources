66903a2589d36c0ca4d77c8bdbde69ca84425a62
ielole <ieloletin@gmail.com>
2017-01-09 13:45:15 -0500

    Adds item to DS store and list/:id view.
    Use Ember-data, DS, to expect item and allows it to belong to list.
    List now  `DS.hasMany` items as well. Each list view ('list/:id')
    uses the `listr-list` component chain to render a singular list.

--- app/list/model.js
+++ app/list/model.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   hidden: DS.attr('boolean'),
-  stanleysAttr: DS.attr('string'),
+  // stanleysAttr: DS.attr('string'),
+  items: DS.hasMany('item'),
 });
